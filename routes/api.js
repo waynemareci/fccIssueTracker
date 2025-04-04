c0f31cddc445afe0c3eb7e14ad2495d308539c4c
waynemareci <wayne@mareci.com>
2025-04-04 14:43:31 +0000

    tests 1 and 2 pass

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,29 @@
-'use strict';
+'use strict'
 
 module.exports = function (app) {
+  app
+    .route('/api/issues/:project')
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-      
+    .get(function (req, res) {
+      let project = req.params.project
     })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
+
+    .post(function (req, res) {
+      let project = req.params.project
+      console.log('processing post; project is ' + project)
+      console.log('test data is ' + req.body.issue_title)
+      res.json({
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by
+      })
     })
-    
-    .put(function (req, res){
-      let project = req.params.project;
-      
+
+    .put(function (req, res) {
+      let project = req.params.project
     })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      
-    });
-    
-};
+
+    .delete(function (req, res) {
+      let project = req.params.project
+    })
+}
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,20 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
-const assert = chai.assert;
-const server = require('../server');
+const chaiHttp = require('chai-http')
+const chai = require('chai')
+const assert = chai.assert
+const server = require('../server')
+const res = require('express/lib/response')
 
-chai.use(chaiHttp);
+chai.use(chaiHttp)
 
-suite('Functional Tests', function() {
-  
-});
+suite('Functional Tests', function () {
+  test('Create an issue with every field: POST request to /api/issues/{project}', done => {
+    chai
+      .request(server)
+      .put('')
+      .send()
+      .end((err, res) => {
+        assert.strictEqual(res.body.issue_title, undefined)
+        done()
+      })
+  })
+})
