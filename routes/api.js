27c728d6013b94c4db57550e58d713e58ba72c88
waynemareci <wayne@mareci.com>
2025-04-04 22:13:52 +0000

    incorporating mongoose/mongodb

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,19 @@
 'use strict'
 
 module.exports = function (app) {
+  const mongoose = require('mongoose')
+  const Schema = mongoose.Schema
+  const issueSchema = new Schema({
+    issue_title: { type: String, required: true },
+    issue_text: { type: String, required: true },
+    created_on: { type: Date, required: true, default: Date.now() },
+    updated_on: { type: Date, required: true, default: Date.now() },
+    created_by: { type: String },
+    assigned_to: { type: String },
+    open: { type: Boolean, required: true, default: true },
+    status_text: { type: String }
+  })
+  const Issue = mongoose.model('Issue', issueSchema)
   app
     .route('/api/issues/:project')
 
@@ -10,6 +23,7 @@ module.exports = function (app) {
 
     .post(function (req, res) {
       let project = req.params.project
+      const instance = new Issue()
       console.log('processing post; project is ' + project)
       console.log('test data is ' + req.body.issue_title)
       res.json({
@@ -21,7 +35,7 @@ module.exports = function (app) {
         created_on: Date.now(),
         updated_on: Date.now(),
         open: true,
-        _id: "someId"
+        _id: 'someId'
       })
     })
 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const expect      = require('chai').expect;
 const cors        = require('cors');
+const mongoose    = require('mongoose');
 require('dotenv').config();
 
 const apiRoutes         = require('./routes/api.js');
@@ -16,7 +17,12 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 
-
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to the Database!"))
+  .catch(err => {
+    console.log(`Could not establish Connection with err: ${err}`);
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
